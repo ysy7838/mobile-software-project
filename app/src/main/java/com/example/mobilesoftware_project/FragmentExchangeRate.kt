ac38306f24695f07946459925f761146db2e8906
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2023-12-05 18:28:53 +0900

    환율 페이지를 위해 생성 및 수정 (미완)

--- app/src/main/java/com/example/mobilesoftware_project/FragmentExchangeRate.kt
+++ app/src/main/java/com/example/mobilesoftware_project/FragmentExchangeRate.kt
@@ -6,12 +6,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.mobilesoftware_project.databinding.FragmentExchangeRateBinding
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 
 /*
     환율 계산기를 보여주는 Fragment
  */
+// 인증키: YvYaRkYwmbIoYa2LQOqGWO0JWJyyiN3R
+
+//https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=YvYaRkYwmbIoYa2LQOqGWO0JWJyyiN3R&searchdate=20231201&data=AP01
+
 class FragmentExchangeRate : Fragment() {
     lateinit var eBinding: FragmentExchangeRateBinding
+    inner class NetworkThread: Thread() {
+        override fun run() {
+            val key = "key"
+            val site = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=YvYaRkYwmbIoYa2LQOqGWO0JWJyyiN3R&searchdate=20231201&data=AP01"
+            val mobileOS = "&MobileOS=AND"
+
+            val url = URL(site)
+            val conn = url.openConnection()
+            val input = conn.getInputStream()
+            val isr = InputStreamReader(input)
+            val br = BufferedReader(isr)
+
+            var str: String? = null
+            val buf = StringBuffer()
+
+            do {
+                str = br.readLine()
+                if (str != null) buf.append(str)
+            } while (str != null)
+
+            val root = JSONObject(buf.toString())
+            val response = root.getJSONObject("response").getJSONObject("body").getJSONObject("items")
+            val item = response.getJSONArray("item")
+
+            for (i in 0 until item.length()) {
+                val jObject = item.getJSONObject(i)
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
