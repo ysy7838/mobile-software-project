4bfdc9953dc2c9796961d401e221d16f70ec5f56
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2023-12-23 01:09:01 +0900

    나갔다가 다시 들어갔을 때 프래그먼트가 출력되지 않는 현상 수정

--- app/src/main/java/com/example/mobilesoftware_project/PageThreetoSixActivity.kt
+++ app/src/main/java/com/example/mobilesoftware_project/PageThreetoSixActivity.kt
@@ -9,8 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.mobilesoftware_project.databinding.PageThreeMainBinding
 import java.util.ArrayList
 import androidx.annotation.RequiresApi
-/*
 import org.json.JSONArray
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,12 +21,6 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 
- */
-
-private const val CHECK = 0         // 프래그먼트를 선택할 때 사용하기 위해 선언
-private const val SCHEDULE = 1
-private const val WEATHER = 2
-private const val EXCHANGE = 3
 
 var fragmentCheck: FragmentCheck? = null
 var fragmentSchedule: FragmentSchedule? = null
@@ -35,7 +29,7 @@ var fragmentExchangeRate: FragmentExchangeRate? = null
 
 class PageThreetoSixActivity : AppCompatActivity() {
 
-    lateinit var binding: PageThreeMainBinding
+    private lateinit var binding: PageThreeMainBinding
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,39 +41,12 @@ class PageThreetoSixActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)     // toolbar 기본 타이틀 안 보이게
         supportActionBar?.setDisplayHomeAsUpEnabled(true)       // 뒤로 가기 버튼을 만듦
 
-        val activityIndex: ArrayList<String>? =
-            intent.getStringArrayListExtra("activityIndex")        // 데이터 전부 받아오기
-        val activityValue: ArrayList<String>? = intent.getStringArrayListExtra("activityValue")
-        val sex = intent.getStringExtra("sex")
-        val destination = intent.getBooleanExtra("destination", false)
-        val isDomestic = intent.getBooleanExtra("isDomestic", false)
-        val isInternational = intent.getStringExtra("isInternational")
-        val tripStart = intent.getStringExtra("tripStart")
-        val tripEnd = intent.getStringExtra("tripEnd")
-        val haveChild = intent.getBooleanExtra("haveChild", false)
+        fragmentCheck = null
+        fragmentSchedule = null
+        fragmentWeather = null
+        fragmentExchangeRate = null
 
-        /*
-        var calendar = Calendar.getInstance()
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-
-        val date = Date()
-        Log.d("exchangecheck", "${dateFormat.format(date)}")
-        val dateList : MutableList<SimpleDateFormat> = mutableListOf()
-
-        val networkService : RetrofitInterface = RetrofitConnection.getInstance().create(RetrofitInterface::class.java)
-        val getResult : Call<List<ClassExchange>> = networkService.doGetResult("YvYaRkYwmbIoYa2LQOqGWO0JWJyyiN3R", "$dateFormat.format(date)", "AP01")
-        getResult.enqueue(object: Callback<List<ClassExchange>> {
-            override fun onResponse(call: Call<List<ClassExchange>>, response: Response<List<ClassExchange>>) {
-                Log.d("exchangecheck", "Success")
-                val getList = response.body()
-                Log.d("exchangecheck", "${getList}")
-            }
-            override fun onFailure(call: Call<List<ClassExchange>>, t: Throwable) {
-                call.cancel()
-                Log.d("exchangecheck", "t.message = ${t.message}")
-                Log.d("exchangecheck", "t.cause = ${t.cause}")
-            }
-        })
+        val filename = intent.getStringExtra("filename")
 
         val exchangeTotal = arrayListOf<dateExchange>()
         val country = arrayListOf<ClassExchange>()
@@ -89,34 +56,24 @@ class PageThreetoSixActivity : AppCompatActivity() {
         for (i in 0 until jsonArray.length()) {
             val jsonObject = jsonArray.getJSONObject(i)
             val unit = jsonObject.getString("cur_unit")
-            val deal = (jsonObject.getString("deal_bas_r")).toDouble()
+            val deal = (jsonObject.getString("deal_bas_r"))
+                .replace(",", "")
             val name = jsonObject.getString("cur_nm")
 
             country.add(ClassExchange(unit, deal, name))
-            Log.d("exchangecheck", "$country")
         }
         exchangeTotal.add(dateExchange("2023.12.01", country))
-        Log.d("exchangecheck", "$country")
-
-         */
-
-
-        initBottomNavigation()
 
         var bundle = Bundle()
-
-        bundle.putStringArrayList("activityIndex", activityIndex)
-        bundle.putStringArrayList("activityValue", activityValue)
-        bundle.putString("sex", sex)
-        bundle.putString("isInternational", isInternational)
-        bundle.putBoolean("haveChild", haveChild)
-
+        bundle.putString("filename", filename)
+        initBottomNavigation(bundle)
         fragmentCheck!!.arguments = bundle
+
         supportFragmentManager!!.beginTransaction()
             .commit()
     }
 
-    fun initBottomNavigation() {
+    fun initBottomNavigation(bundle : Bundle) {
         fragmentCheck = FragmentCheck()
         supportFragmentManager.beginTransaction()
             .add(R.id.menu_frame_layout, fragmentCheck!!)
@@ -135,6 +92,7 @@ class PageThreetoSixActivity : AppCompatActivity() {
                     if (fragmentSchedule != null) trans.hide(fragmentSchedule!!)
                     if (fragmentWeather != null) trans.hide(fragmentWeather!!)
                     if (fragmentExchangeRate != null) trans.hide(fragmentExchangeRate!!)
+
                     trans.commit()
                 }
 
@@ -142,6 +100,7 @@ class PageThreetoSixActivity : AppCompatActivity() {
                     binding.ToolbarTitle.text = getString(R.string.SchedulePage)
                     if (fragmentSchedule == null) {
                         fragmentSchedule = FragmentSchedule()
+                        fragmentSchedule?.arguments = bundle
                         trans.add(R.id.menu_frame_layout, fragmentSchedule!!)
                     }
                     if (fragmentCheck != null) trans.hide(fragmentCheck!!)
@@ -155,6 +114,7 @@ class PageThreetoSixActivity : AppCompatActivity() {
                     binding.ToolbarTitle.text = getString(R.string.WeatherPage)
                     if (fragmentWeather == null) {
                         fragmentWeather = FragmentWeather()
+                        fragmentWeather?.arguments = bundle
                         trans.add(R.id.menu_frame_layout, fragmentWeather!!)
                     }
                     if (fragmentCheck != null) trans.hide(fragmentCheck!!)
@@ -168,6 +128,7 @@ class PageThreetoSixActivity : AppCompatActivity() {
                     binding.ToolbarTitle.text = getString(R.string.ExchangePage)
                     if (fragmentExchangeRate == null) {
                         fragmentExchangeRate = FragmentExchangeRate()
+                        fragmentExchangeRate?.arguments = bundle
                         trans.add(R.id.menu_frame_layout, fragmentExchangeRate!!)
                     }
                     if (fragmentCheck != null) trans.hide(fragmentCheck!!)
@@ -196,8 +157,9 @@ class PageThreetoSixActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {       // 툴바에 있는 게 선택 되었을 때
-        when (item.itemId) {                         // 뒤로가기 버튼이 선택되었을 때
-            android.R.id.home -> {                  // 여기에서 지금까지 한 거 저장하고 finish 해야 함
+
+        when (item.itemId) {
+            android.R.id.home -> {                    // 뒤로가기 버튼이 선택되었을 때
                 Log.d("incomplete", "home") // incomplete 되어있는 부분은 추가가 필요
                 finish()
                 return true
@@ -213,6 +175,8 @@ class PageThreetoSixActivity : AppCompatActivity() {
 
             R.id.topMenu_trip_delete -> {          // 활동을 삭제할 수 있도록
                 Log.d("incomplete", "trip_delete")
+
+                finish()
             }
         }
         return super.onOptionsItemSelected(item)
